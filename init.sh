1e07a6fb96844061baf5eb55e086d0ed01e56b9e
Sam Moes <sam.moes15@gmail.com>
2022-04-02 01:25:22 +0200

    added .gitignore

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# epg-wrapper" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/le3ch-tech/epg-wrapper.git
-git push -u origin main
